03b5e421cdbaa7bc9caf28ab7809423538e42132
SeaOreos <iadegoroye@guilford.edu>
2024-09-19 13:36:13 -0400

    Finished but one question: since the number of sides inputted by the player is being used in the bound, wouldn't that mean that the number of sides rolled is actually one less? Example: If i were to type in 6 sides, the actual number of sides would be 5 because the options that could be rolled would be (1,6) allowing for number 1,2,3,4, and 5 to be rolled. Was this a mistake on my part? If so, how do I fix it? Would I just add a plus one or is the solution something else? Thank you for everything as always :DDDDD

--- src/main/java/edu/guilford/Main.java
+++ src/main/java/edu/guilford/Main.java
@@ -1,11 +1,27 @@
 package edu.guilford;
+//Dara Adegoroye (SeaOreos) 09/19/24
+import java.util.Random; //IMPORTS THE LIBRARY WITH THE RANDOM COMMANDS
+import java.util.Scanner; //IMPORTS THE LIBRARY WITH THE SCANNER COMMANDS
 
 public class Main {
+    private static final Random rand = new Random(); //DECLARES A NEW VARIABLE FOR RANDOM NUMBERS CALLED RANDOM
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in); //DECLARES A NEW VARIABLE FOR THE SCANNER CALLED SCAN
+        System.out.print("HOWDY TRAVELER! YOU MUST ROLL TEN DIE OR YOU WILL ROLL AND D13! HOW MANY SIDES? ");
+        int sides = scan.nextInt(); //WHENEVER SIDES IS USED LATER IN THE CODE, IT USES THE NUMBER INPUTTED BY THE PLAYER
+        System.out.print("HOW MANY DIE ARE YOU ROLLING??? ");
+        int number = scan.nextInt(); //WHENEVER NUMBER IS USED LATER IN THE CODE, IT USES THE NUMBER INPUTTED BY THE PLAYER
+        int sum = 0; //THE SUM STARTS AT ZERO BEFORE THE OTHER RESULTS ARE ADDED
         // default for loop, prints loop variable as it counts up
-        for (int i = 0; i < 10; i++) {
-            System.out.println("loop variable i = " + i);
+        for (int i = 0; i < number; i++) { //CONDITIONS FOR THE FOR LOOP; i IS DECLARED TO START AT ZERO; i MUST BE LESS THAN THE NUMBER THAT THE PLAYER INPUTTED FOR NUMBER OF DIE BEING ROLLED; KEEPS ADDING ONE VALUE TO i UNTIL IT EXCEEDS NUMBER
+            int diceRoll = rand.nextInt(1, sides);
+           sum += diceRoll; //ADDS UP ALL OF THE DIE BEFORE THIS POINT
+           System.out.println("You rolled a " + diceRoll); //HI I WANTED THIS HERE TO CHECK IF THE ADDING THING WAS RIGHT 💃
+           //System.out.println("sum of " + i + "d" + sides + " = " + sum);
+           
         }
+        System.out.println(number + "d" + sides + "=" + sum); 
+        scan.close(); 
     }
+    
 }
